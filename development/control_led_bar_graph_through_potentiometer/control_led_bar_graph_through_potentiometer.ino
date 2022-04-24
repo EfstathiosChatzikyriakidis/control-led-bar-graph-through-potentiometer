1ba7c69b8a823e54ce03c8699ab954472d6eb573
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:27:18 +0300

    Change.

--- development/control_led_bar_graph_through_potentiometer/control_led_bar_graph_through_potentiometer.ino
+++ development/control_led_bar_graph_through_potentiometer/control_led_bar_graph_through_potentiometer.ino
@@ -1,7 +1,7 @@
 /*
  *  Control LED Bar Graph Through Potentiometer.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
